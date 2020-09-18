c8b37426dcf37be3dc52ae5cc8d0b8a8c8f3e681
jigarbytes <41102183+jigarbytes@users.noreply.github.com>
2020-09-18 12:53:11 +0530

    Add files via upload

---
+++ agttest.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: AGT AUTO UPDATE
+Plugin URI: http://www.bytestechnolab.com/finale3d
+description: a plugin to create awesomeness and spread joy
+Version: 1.0.0
+Author: BytesTeam
+Text Domain: finale3d
+Domain Path: languages
+Author URI: http://bytestechnolab.com
+License: GPL2
+*/
+
+if (!defined('ABSPATH')){
+	die('<h3>Direct access to this file do not allow!</h3>');
+}
+
+if (!function_exists('get_plugin_data')) {
+	require_once (ABSPATH . 'wp-admin/includes/plugin.php');
+}
+
+
+require 'vendor/autoload.php';
